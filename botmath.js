81ee6803322bb3955395138e09458f67f81deb50
yanege <ext-georgi.yanev@f-secure.com>
2016-02-23 14:34:10 +0200

    fix merge conflicts

--- botmath.js
+++ botmath.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 var isPrime = function (n) {
- if (isNaN(n) || !isFinite(n) || n%1 || n<2) return false; 
+ if (isNaN(n) || !isFinite(n) || n%1 || n<2) return false;
  if (n%2==0) return (n==2);
  var m=Math.sqrt(n);
  for (var i=3;i<=m;i+=2) {
@@ -11,10 +10,8 @@ var isPrime = function (n) {
 
 module.exports.isPrime = isPrime;
 
-=======
 var sum = function (num1, num2) {
 	return parseFloat(num1) + parseFloat(num2);
 }
 
 module.exports.sum = sum;
->>>>>>> test
